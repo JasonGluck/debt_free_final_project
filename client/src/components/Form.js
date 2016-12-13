1b19b9339f52659cf6243eb93851a6dd631039ca
lshapz <laurashapiro01@gmail.com>
2016-12-13 13:32:42 -0500

    event.target. shhhh

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -8,9 +8,9 @@ import {browserHistory} from 'react-router'
 const Form = (props) => {
 
   const handleChange = (event) => {
-    debugger
     let result = {}; let id = event.target.id;
     let val = event.target.value ? parseFloat(event.target.value) : 0
+    // debugger
     result[id] = val
     props.setValue(result)
   }
@@ -50,7 +50,7 @@ const Form = (props) => {
         <h2>Default Financial Data</h2>
         {props.data.payment &&
         <div>
-          <label id="userLabel">Monthly Payment: </label>$<input><InputBoxDoneTyping id="payment" defaultValue={+(props.data.payment).toFixed(2)} placeholder="i.e.123.45" doneTyping={handleChange.bind(props)} doneTypingInterval={300} /></input>
+          <label id="userLabel">Monthly Payment: </label>$<InputBoxDoneTyping id="payment" defaultValue={+(props.data.payment).toFixed(2)} placeholder="i.e.123.45" doneTyping={handleChange.bind(props)} doneTypingInterval={300} />
           <label id="userLabel">Monthly Expenditure: </label>$<InputBoxDoneTyping id="expenditure" defaultValue={+(props.data.expenditure).toFixed(2)} placeholder="i.e.123.45" doneTyping={handleChange.bind(props)} doneTypingInterval={300} />
         </div>}
       </div>
