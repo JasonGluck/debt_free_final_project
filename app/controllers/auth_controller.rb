e86f66924d523b56422bd6dced1bbb6479e82b11
lshapz <laurashapiro01@gmail.com>
2016-12-14 10:37:58 -0500

    circle

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,16 +1,19 @@
 class AuthController < ApplicationController
   def authenticate
+    # byebug
     user = User.find_by_credentials(params[:email], params[:password])
     if user
+      # byebug
       render json: authentication_payload(user)
     else
-      render json: { errors: ['Invalid email or password'] }, status: :unauthorized
+      render json: { errors: ['FOOOO'] }, status: :unauthorized
     end
   end
 
   private
 
   def authentication_payload(user)
+    # byebug
     return nil unless user && user.id
     {git
       auth_token: AuthToken.encode({ user_id: id }),
