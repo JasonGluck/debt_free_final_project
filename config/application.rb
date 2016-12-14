14905af0073fe4486bcaaa9cb509abce36b74da4
lshapz <laurashapiro01@gmail.com>
2016-12-13 19:34:07 -0500

    require auth?

--- config/application.rb
+++ config/application.rb
@@ -27,10 +27,10 @@ module DebtFreeFinalProject
       end
     end
 
-  config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => '*',
-    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
-  }
+  # config.action_dispatch.default_headers = {
+  #   'Access-Control-Allow-Origin' => '*',
+  #   'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+  # }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
