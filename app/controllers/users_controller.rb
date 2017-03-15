22b70cd645215de836c8fbe05bfba280751c17f6
pgaret <peregringaret@gmail.com>
2017-03-15 01:23:06 -0400

    Encode rather than issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      jwt = JWT.issue({user_id: user.id})
+      jwt = JWT.encode {user_id: user.id}, nil, 'none'
       render json: {jwt: jwt, user: user}
     else
       render status: 404, json: {error: user.errors.full_messages}
